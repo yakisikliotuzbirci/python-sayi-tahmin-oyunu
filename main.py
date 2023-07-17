b44029f0455c11f274973f2264ef758ab2ac2997
SametSever <139774058+SametSever@users.noreply.github.com>
2023-07-17 18:44:29 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import random
+
+sayi = random.randint(1,100)
+hak = 5
+tahmin : int
+
+while hak > 0:
+    hak = hak - 1
+    tahmin = int(input("Tahmininizi giriniz : "))
+
+    if sayi == tahmin:
+        print("Tebrikler, bildiniz.")
+        break
+
+    if sayi > tahmin:
+        print("yukarı")
+    else:
+        print("aşağı")
+
+    if hak == 0:
+        print(f"Hakkınız bitti. Tutulan sayı {sayi}.")
+        break
