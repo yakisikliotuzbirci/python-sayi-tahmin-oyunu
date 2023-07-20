52b6d78f41065fdf1fb7f1262a14823eda8323bc
SametSever <139774058+SametSever@users.noreply.github.com>
2023-07-20 13:21:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import random
+def main():
+    sayi = random.randint(1,100)
+    hak = 5
+    tahmin : int
 
-sayi = random.randint(1,100)
-hak = 5
-tahmin : int
+    while hak > 0:
+        hak = hak - 1
+        tahmin = int(input("Tahmininizi giriniz : "))
 
-while hak > 0:
-    hak = hak - 1
-    tahmin = int(input("Tahmininizi giriniz : "))
+        if sayi == tahmin:
+            print("Tebrikler, bildiniz.")
+            break
 
-    if sayi == tahmin:
-        print("Tebrikler, bildiniz.")
-        break
+        if sayi > tahmin:
+            print("yukarı")
+        else:
+            print("aşağı")
 
-    if sayi > tahmin:
-        print("yukarı")
-    else:
-        print("aşağı")
+        if hak == 0:
+            print(f"Hakkınız bitti. Tutulan sayı {sayi}.")
+            break
 
-    if hak == 0:
-        print(f"Hakkınız bitti. Tutulan sayı {sayi}.")
-        break
+if __name__ == "__main__"
+    main()
